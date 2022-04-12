e62852794520f47394a6f57de571ff4d5e6916c7
renhz2017 <blestmilk@hotmail.com>
2022-04-12 17:03:49 +0800

    tsconfig

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,28 @@
-﻿const greet = (myName: string): string => `Hello ${myName}`;
+﻿const greet = (myName: string): string => {
+  interface GameSettings {
+    speed: "fast" | "medium" | "slow";
+    quality: "high" | "low";
+    [key: string]: string;
+  }
+
+  let mySettings : GameSettings = {
+    speed: "fast",
+    quality:"high"
+  };
+
+  const mySpeed = mySettings.speed;
+  const myQuality = mySettings.quality;
+  const myUsername = mySettings.username;
+
+  let greetwords: string;
+  switch (myName.length > 4) {
+    case true:
+      greetwords = `Hi, ${myName}`;
+    case false:
+      greetwords = `Hello, ${myName}`;
+    default:
+        greetwords = 'Hello, dear'
+  }
+  return greetwords;
+};
 export default greet;
