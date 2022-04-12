1e7387aeb257cd5e4de0d9c43bba42cf1119cb68
renhz2017 <blestmilk@hotmail.com>
2022-04-12 10:28:50 +0800

    jest

---
+++ tsconfig-paths-bootstrap.js
@@ -0,0 +1,9 @@
+﻿// https://gist.github.com/jmcdo29/6276920d3dcbc1161c32a7a9a461f9c1
+const tsconfigPaths = require('tsconfig-paths');
+
+const config = require('./tsconfig.json');
+
+tsconfigPaths.register({
+  baseUrl: config.compilerOptions.baseUrl,
+  paths: config.compilerOptions.paths,
+});
